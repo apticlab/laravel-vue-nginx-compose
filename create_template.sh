ac587ad46bf0e80d11afc32e9471bd230b32f3c8
Giacomo Alberini <giacomoalbe@gmail.com>
2020-06-17 18:29:32 +0200

    Add template for composer.json & package.json

--- create_template.sh
+++ create_template.sh
@@ -6,6 +6,10 @@ echo "Creating docker-compose template..."
 envsubst < ./templates/docker-compose.template.yml > docker-compose.yml
 echo "Creating docker-compose.prod template..."
 envsubst < ./templates/docker-compose.prod.template.yml > docker-compose.prod.yml
+echo "Creating package.json template..."
+envsubst < ./templates/package.template.json > ./client/package.json
+echo "Creating composer.json template..."
+envsubst < ./templates/composer.template.json > ./server/composer.json
 echo "Creating nginx template..."
 envsubst < ./templates/nginx.dev.template.conf > ./web/nginx.dev.conf
 
